854fc9911e16b3ab537dce7f73be109582280ee3
Sergiu Tirsina <sergiu.tirsina@amdaris.com>
2020-08-15 11:26:56 +0300

    dgdgdfg

--- backend/layers/helper_lib/src/token-decoder.js
+++ backend/layers/helper_lib/src/token-decoder.js
@@ -1,6 +1,8 @@
 var jwt = require('jsonwebtoken');
 const { PEM_KEY } = require('./constants');
 
+//
+
 
 function getUserId(event) {
     const token = event.headers['Authorization'].replace(/['"]+/g, '');
